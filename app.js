84890156593e40f1f6e1fe3ca23c382a9d3f6909
joe-ashwell <67185069+joe-ashwell@users.noreply.github.com>
2020-08-18 18:35:00 +0200

    Include date and time as a string

--- app.js
+++ app.js
@@ -3,8 +3,14 @@ const minuteHand = document.querySelector('.minute-hand');
 const hourHand = document.querySelector('.hour-hand');
 const h2 = document.querySelector('h2.get-time');
 
+const monthsOfYear = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
+
 let setDate = () => {
 
+  let updateMinutes;
+  let updateSeconds;
+  let updateHours;
   const now = new Date();
   const year = now.getFullYear();
   const month = now.getUTCMonth();
@@ -22,7 +28,35 @@ let setDate = () => {
   const hoursDegrees = (((hours / 12) * 360) + 90 );
   hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
   
-  h2.innerHTML = `${hours}:${minutes}:${seconds} <br> ${day}/${month + 1}/${year}`;
+  if ( minutes < 10 ) {
+
+    updateMinutes = `0${minutes}`;
+
+  } else {
+
+    updateMinutes = `${minutes}`;
+
+  }
+  if ( seconds < 10 ) {
+
+    updateSeconds = `0${seconds}`;
+
+  } else {
+
+    updateSeconds = `${seconds}`;
+
+  }
+  if ( hours < 10 ) {
+
+    updateHours = `0${hours}`;
+
+  } else {
+
+    updateHours = `${hours}`;
+
+  }
+  
+  h2.innerHTML = `${updateHours}:${updateMinutes}:${updateSeconds} <br> ${day} ${monthsOfYear[month]} ${year}`;
 
 }
 
